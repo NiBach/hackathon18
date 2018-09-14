e8aa7ef9fe5e304ea86c4dffdedc439e103aa879
Ido Salomon <idosalomon@gmail.com>
2018-09-14 03:12:39 +0300

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import logo from '../assets/LogoTeal.png';
+
+//import Icon from 'react-materialize'
 
 const NavBar = ({ addToList }) => {
     return (
         <div>
             <nav id='nav-id'>
-                <div className="nav-wrapper blue">
-                    <a className="brand-logo">Logo</a>
+                <div className="nav-wrapper teal darken-1">
+
+                    <a className="brand-logo right"><img src={logo} height='54'></img></a>
                     <a data-target="slide-out" className="sidenav-trigger hide-on-large-only left">
                         <i className="material-icons">menu</i>
                     </a>
+                    <Link className="brand-logo btn-floating btn-large waves-effect waves-light red center" to="/cameraView"><i className="material-icons teal lighten-4">where_to_vote</i></Link>
+
                 </div>
             </nav>
-            
+
         </div>
     )
 }
 
 export default NavBar
-
-
-
