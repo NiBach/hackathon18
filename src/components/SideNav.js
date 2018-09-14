0cd378d445fca852f54ebb8e7960bec1567d6c49
Noah Harel <noaharel01@gmail.com>
2018-09-14 08:15:44 +0300

    remove photo

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -12,10 +12,10 @@ function sidenav ({imageURL, name}) {
                     <img src="https://media-cdn.tripadvisor.com/media/photo-s/13/78/52/51/wild-food-foraging.jpg"/>
                     
                 </div>
-                
-                <a><img className="circle" src={imageURL}/></a>
-                <a><span className="white-text name">{name}</span></a>
-                <a><span className="white-text name">Level: Sprout</span></a>                
+                <h2 style={{ padding: "10px 20px"}}> </h2>
+               
+                <a><span className="white-text name"><h5>Happy Foraging, {name}!</h5></span></a>
+                <a><span className="white-text name"><h6>Level: Sprout</h6></span></a>                
                 </div></li>
             <li><Link className='sidenav-close' to="/"><i class="material-icons">map</i>cEATy map</Link></li>
             <li><div class="divider"></div></li>
